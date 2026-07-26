7a9d7aaf5bb5d6967876d863416b466159c7e58a
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-26 07:05:30 +0530

    Day01 second question

--- Day01.js
+++ Day01.js
@@ -28,3 +28,26 @@ console.log("Age:", age);
 console.log("City:", city);
 
 
+// 🔹 Question 2 – Difference Between let, const, and var
+// Write a JavaScript program that:
+// Creates a let variable named age with value 22.
+// Changes its value to 23.
+// Creates a const variable named country with value "India".
+// Displays both values using console.log().
+// Expected Output
+// Age: 23
+// Country: India
+// ⚠️ Conditions
+// ✅ Use one let variable
+// ✅ Update the let variable
+// ✅ Use one const variable
+// ✅ Use console.log()
+// ❌ Don't use var
+// ❌ Don't try to change the const variable
+
+let age1 = 22;
+age1 = 23;
+const country = "India";
+
+console.log("Age:",age1);
+console.log("Country:",country);
