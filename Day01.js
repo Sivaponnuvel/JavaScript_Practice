e10b4feb25ff5e5aa1c60531cd7a5882c8092087
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-26 07:03:57 +0530

    Day01 first question

---
+++ Day01.js
@@ -0,0 +1,30 @@
+// 🔹 Question 1 – Display Personal Details
+// Write a JavaScript program to display the following details using variables and console.log().
+// Program Flow
+// Create variables for:
+// Name
+// Age
+// City
+// Display the output like this:
+// Name: Siva
+// Age: 22
+// City: Chennai
+// ⚠️ Conditions
+// ✅ Use let for all variables
+// ✅ Use console.log()
+// ✅ Print each detail on a separate line
+// ❌ Don't use var
+// ❌ Don't hardcode the entire sentence directly inside console.log()
+// Example (❌ Wrong):
+// console.log("Name: Siva");
+// Instead, do something like:
+// let name = "Siva";
+// console.log("Name:", name);
+
+let name = "Siva", age = 22, city = "Chennai";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+
+
