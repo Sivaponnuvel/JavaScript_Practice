cc15ff23e0061ed725b0440c7a02d0579c3ac4de
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-29 09:12:46 +0530

    Day04 first question

---
+++ Day04.js
@@ -0,0 +1,40 @@
+// 🔹 Question 1 – Compare Two Numbers
+// Write a JavaScript program to compare two numbers.
+// Program Flow
+// Create two variables:
+// num1 = 15
+// num2 = 20
+// Check and display the results of the following comparison operators:
+// Greater than (>)
+// Less than (<)
+// Greater than or equal to (>=)
+// Less than or equal to (<=)
+// Equal to (==)
+// Strict equal to (===)
+// Not equal to (!=)
+// Strict not equal to (!==)
+// Example Output
+// Greater Than: false
+// Less Than: true
+// ...
+// ⚠️ Conditions
+// ✅ Use let for both variables
+// ✅ Use all comparison operators listed above
+// ✅ Display each result using console.log()
+// ❌ Don't use var
+// ❌ Don't hardcode the answers
+// 💡 Hint: Every comparison operator returns either true or false.
+
+let num1 = 15;
+let num2 = 20;
+
+console.log("Greater Than:", num1 > num2);
+console.log("Less Than:", num1 < num2);
+console.log("Greater than or equal to:", num1 >= num2);
+console.log("Less than or equal to:", num1 <= num2);
+console.log("Equal to:", num1 == num2);
+console.log("Strict equal to:", num1 === num2);
+console.log("Not equal to:", num1 != num2);
+console.log("Strict Not equal to:", num1 !== num2);
+
+
