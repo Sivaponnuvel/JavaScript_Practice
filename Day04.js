38d84ae50fb86d444972d0698c79b12e205df707
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-07-29 09:13:14 +0530

    Day04 second question

--- Day04.js
+++ Day04.js
@@ -38,3 +38,33 @@ console.log("Not equal to:", num1 != num2);
 console.log("Strict Not equal to:", num1 !== num2);
 
 
+// 🔹 Question 2 – Check Eligibility Using Logical Operators
+// Write a JavaScript program to check whether a person is eligible to apply for a job.
+// Program Flow
+// Create the following variables:
+// Age = 22
+// HasDegree = true
+// Display whether the person is eligible based on these conditions:
+// Age should be 18 or above
+// HasDegree should be true
+// If both conditions are satisfied, display:
+// Eligible for Job
+// Otherwise, display:
+// Not Eligible for Job
+// ⚠️ Conditions
+// ✅ Use && (Logical AND)
+// ✅ Use one if...else statement
+// ✅ Use console.log() for the output
+// ❌ Don't use nested if
+// ❌ Don't hardcode the output
+// 💡 Hint: Think about how to combine two conditions into a single expression.
+
+let Age = 22;
+let HasDegree = true;
+
+if(Age >= 18 && HasDegree){
+    console.log("Eligible for Job");
+}
+else{
+    console.log("Not Eligible for Job");
+}
