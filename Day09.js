8f6d3c356bb82648ab9cfd0b4a2a1c5ddaac214b
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-03 09:59:15 +0530

    Day09 second question

--- Day09.js
+++ Day09.js
@@ -25,3 +25,31 @@ for(let i = 1; i <= 10; i++){
 }
 
 
+// 🔹 Question 2 – Print Even Numbers from 2 to 20
+// Write a JavaScript program to print all even numbers from 2 to 20.
+// Program Flow
+// Use a for loop.
+// Print only even numbers.
+// Example Output
+// 2
+// 4
+// 6
+// 8
+// 10
+// 12
+// 14
+// 16
+// 18
+// 20
+// ⚠️ Conditions
+// ✅ Use a for loop
+// ✅ Use console.log()
+// ❌ Don't print odd numbers
+// ❌ Don't write each number manually
+// 💡 Hint: There are multiple ways to print even numbers. Choose the one you think is simplest.
+
+for (let i = 1; i <= 20; i++){
+    if (i % 2 === 0){
+        console.log(i);
+    }
+}
