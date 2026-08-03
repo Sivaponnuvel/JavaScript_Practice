f7e46152228adebf0b374568cd17468f8f577751
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-03 09:58:46 +0530

    Day09 first question

---
+++ Day09.js
@@ -0,0 +1,27 @@
+// 🔹 Question 1 – Print Numbers from 1 to 10
+// Write a JavaScript program to print numbers from 1 to 10.
+// Program Flow
+// Use a for loop.
+// Print each number on a separate line.
+// Example Output
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// ⚠️ Conditions
+// ✅ Use a for loop
+// ✅ Use console.log()
+// ❌ Don't write 10 console.log() statements
+// 💡 Hint: Think about the three parts of a for loop: initialization, condition, and update.
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+
