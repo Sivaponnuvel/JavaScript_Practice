450c3e64a693a93e8c2ed4ddee10e63532920512
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-05 01:05:05 +0530

    Day11 first question

---
+++ Day11.js
@@ -0,0 +1,34 @@
+// 🔹 Question 1 – Print Numbers from 1 to 10
+// Write a JavaScript program to print numbers from 1 to 10 using a while loop.
+// Program Flow
+// Create a variable with the starting value.
+// Use a while loop.
+// Print each number on a separate line.
+// Update the variable inside the loop.
+// Example Output
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// ⚠️ Conditions
+// ✅ Use a while loop
+// ✅ Use console.log()
+// ✅ Update the loop variable inside the loop
+// ❌ Don't use a for loop
+// ❌ Don't write multiple console.log() statements
+// 💡 Hint: A while loop continues running as long as its condition is true.
+
+let num = 1;
+
+while (num <= 10){
+    console.log(num);
+    num++;
+}
+
+
