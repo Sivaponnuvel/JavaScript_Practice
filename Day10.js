cf89841140dbbca80e69a5b08b2ea8daac57b2a2
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-04 08:37:04 +0530

    Day10 second question

--- Day10.js
+++ Day10.js
@@ -26,3 +26,31 @@ for(let i = 10; i >= 1; i--){
 }
 
 
+// 🔹 Question 2 – Count Multiples of 5 from 1 to 50
+// Write a JavaScript program to print all numbers between 1 and 50 that are divisible by 5.
+// Program Flow
+// Use a for loop.
+// Print each multiple of 5 on a separate line.
+// Example Output
+// 5
+// 10
+// 15
+// 20
+// 25
+// 30
+// 35
+// 40
+// 45
+// 50
+// ⚠️ Conditions
+// ✅ Use a for loop
+// ✅ Use console.log()
+// ❌ Don't write the numbers manually
+// ❌ Don't use arrays
+// 💡 Hint: Use the remainder (%) operator to check divisibility.
+
+for(let i = 1; i <= 50; i++){
+    if (i % 5 === 0){
+        console.log(i);
+    }
+}
