97d42954f8b5d1cfde23c77f3d03336ede5ff2de
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-04 08:36:31 +0530

    Day10 first question

---
+++ Day10.js
@@ -0,0 +1,28 @@
+//  🔹 Question 1 – Print Numbers from 10 to 1
+//  Write a JavaScript program to print numbers from 10 to 1 in reverse order.
+//  Program Flow
+//  Use a for loop.
+//  Print one number per line.
+//  Example Output
+//  10
+//  9
+//  8
+//  7
+//  6
+//  5
+//  4
+//  3
+//  2
+//  1
+//  ⚠️ Conditions
+//  ✅ Use a for loop
+//  ✅ Use console.log()
+//  ❌ Don't use while
+//  ❌ Don't write multiple console.log() statements
+//  💡 Hint: Think about whether the loop variable should increase or decrease.
+
+for(let i = 10; i >= 1; i--){
+    console.log(i);
+}
+
+
