faf8268c09e66c5e15f94d9be827c8f6f160152a
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-07 14:04:04 +0530

    Day13 first question

---
+++ Day13.js
@@ -0,0 +1,35 @@
+// 🔹 Question 1 – Print Numbers from 20 to 10
+// Write a JavaScript program to print numbers from 20 to 10 in reverse order using a while loop.
+// Program Flow
+// Create a variable with the starting value.
+// Use a while loop.
+// Print one number per line.
+// Decrease the variable after each iteration.
+// Example Output
+// 20
+// 19
+// 18
+// 17
+// 16
+// 15
+// 14
+// 13
+// 12
+// 11
+// 10
+// ⚠️ Conditions
+// ✅ Use a while loop
+// ✅ Use console.log()
+// ✅ Update the loop variable inside the loop
+// ❌ Don't use a for loop
+// ❌ Don't write multiple console.log() statements
+// 💡 Hint: Think about how to decrease the loop variable after every iteration.
+
+num = 20
+
+while(num >= 10){
+    console.log(num);
+    num--;
+}
+
+
