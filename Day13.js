9a7429209068206ccc99a15e5a82fdcdcfc07436
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-07 14:04:21 +0530

    Day13 second question

--- Day13.js
+++ Day13.js
@@ -33,3 +33,38 @@ while(num >= 10){
 }
 
 
+// 🔹 Question 2 – Print Multiples of 3 from 3 to 30
+// Write a JavaScript program to print all numbers between 3 and 30 that are divisible by 3 using a while loop.
+// Program Flow
+// Create a variable with the starting value.
+// Use a while loop.
+// Print only multiples of 3.
+// Continue until 30.
+// Example Output
+// 3
+// 6
+// 9
+// 12
+// 15
+// 18
+// 21
+// 24
+// 27
+// 30
+// ⚠️ Conditions
+// ✅ Use a while loop
+// ✅ Use console.log()
+// ✅ Update the loop variable inside the loop
+// ❌ Don't use a for loop
+// ❌ Don't use arrays
+// ❌ Don't write the numbers manually
+// 💡 Hint: Use the remainder (%) operator to check divisibility.
+
+num1 = 3
+
+while (num1 <= 30){
+    if (num1 % 3 === 0){
+        console.log(num1);
+    }
+    num1++;
+}
