02b1eb1fde73285cd8da67116542349f4114ab68
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-08 09:51:44 +0530

    Day14 first question

---
+++ Day14.js
@@ -0,0 +1,39 @@
+// 🔹 Question 1 – Print Numbers from 15 to 1
+// Write a JavaScript program to print numbers from 15 to 1 in reverse order using a do...while loop.
+// Program Flow
+// Create a variable with the starting value.
+// Use a do...while loop.
+// Print one number per line.
+// Decrease the variable after each iteration.
+// Example Output
+// 15
+// 14
+// 13
+// 12
+// 11
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// ⚠️ Conditions
+// ✅ Use a do...while loop
+// ✅ Use console.log()
+// ✅ Update the loop variable inside the loop
+// ❌ Don't use for
+// ❌ Don't use while
+// 💡 Hint: Decrease the variable using the decrement operator (--).
+
+let i = 15;
+
+do{
+    console.log(i);
+    i--;
+}while(i >= 1);
+
+
