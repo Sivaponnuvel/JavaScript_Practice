31bf77250e53623f2d8f41e6bb56a03ca9a47a4d
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-09 03:37:01 +0530

    Day15 second question

--- Day15.js
+++ Day15.js
@@ -28,3 +28,31 @@ for (let i = 1; i <= num; i++){
 }
 
 
+// 🔹 Question 2 – Skip Even Numbers Using continue
+// Write a JavaScript program to print only the odd numbers from 1 to 10.
+// Program Flow
+// Use a for loop.
+// Skip all even numbers.
+// Print only odd numbers.
+// Example Output
+// 1
+// 3
+// 5
+// 7
+// 9
+// ⚠️ Conditions
+// ✅ Use a for loop
+// ✅ Use the continue statement
+// ✅ Use console.log()
+// ❌ Don't use while
+// ❌ Don't use arrays
+// 💡 Hint: When you find an even number, use continue to skip printing it.
+
+let num1 = 10;
+
+for (let i = 1; i <= num1; i++){
+    if(i % 2 === 0){
+        continue;
+    }
+    console.log(i)
+}
