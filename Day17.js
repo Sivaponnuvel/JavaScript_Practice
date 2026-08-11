37403be2d533512ffab0611d136620dcea2373db
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-11 16:00:30 +0530

    Day17 second question

--- Day17.js
+++ Day17.js
@@ -25,3 +25,31 @@ for(let i = 1; i <= 5; i++){
 }
 
 
+// 🔹 Question 2 – Repeated Number Pattern
+// Write a JavaScript program to print the following pattern using nested for loops:
+// Example Output
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// ⚠️ Conditions
+// ✅ Use nested for loops
+// ✅ Outer loop should control the rows
+// ✅ Inner loop should control how many times the number is printed
+// ✅ Use console.log()
+// ❌ Don't write each row manually
+// ❌ Don't use arrays
+// ❌ Don't use while
+// 💡 Hint:
+// For row 1, print 1 one time.
+// For row 2, print 2 two times.
+// For row 3, print 3 three times...
+
+for (let i = 1; i <= 5; i++){
+    let row1 = "";
+    for(let j = 1; j <= i; j++){
+        row1 += i;
+    }
+    console.log(row1)
+}
