4b3f0fde7ae183c50b16130b3b57e64e0b8b4a22
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-12 16:37:14 +0530

    Day18 second question

--- Day18.js
+++ Day18.js
@@ -27,3 +27,30 @@ for (let i = 1; i <= 6; i++){
 }
 
 
+// 🔹 Question 2 – Decreasing Number Pattern
+// Write a JavaScript program using nested for loops to print:
+// Example Output
+// 123456
+// 12345
+// 1234
+// 123
+// 12
+// 1
+// ⚠️ Conditions
+// ✅ Use nested for loops
+// ✅ Outer loop should control the rows
+// ✅ Inner loop should control the numbers
+// ✅ Use console.log()
+// ✅ Build each row before printing it
+// ❌ Don't write each row manually
+// ❌ Don't use arrays
+// ❌ Don't use while
+// 💡 Hint: Unlike Question 1, the number of values in each row should decrease by 1.
+
+for (let i = 6; i >= 1; i--){
+    let pattern1 = ""
+    for (let j = 1; j <= i; j++){
+        pattern1 += j;
+    }
+    console.log(pattern1)
+}
