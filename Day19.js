26657908ddde4f7f16c0b2c2ca249d23677cd3d1
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-13 15:00:56 +0530

    Day19 second question

--- Day19.js
+++ Day19.js
@@ -30,3 +30,30 @@ for(let i = 1; i <= 5; i++){
 }
 
 
+// 🔹 Question 2 – Decreasing Star Pattern
+// Write a JavaScript program using nested for loops to print:
+// Example Output
+// *****
+// ****
+// ***
+// **
+// *
+// ⚠️ Conditions
+// ✅ Use nested for loops
+// ✅ Outer loop should control the rows
+// ✅ Inner loop should control the stars
+// ✅ Use console.log()
+// ✅ Build each row before printing it
+// ❌ Don't write each row manually
+// ❌ Don't use arrays
+// ❌ Don't use while
+// 💡 Hint:
+// Unlike Question 1, the number of stars should decrease by 1 in every row.
+
+for(let i = 5; i >= 1; i--){
+    let pattern1 = "";
+    for(let j = 1; j <= i; j++){
+        pattern1 += "*";
+    }
+    console.log(pattern1);
+}
