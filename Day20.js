ff6982c797724fefc7c4b28100f4336c1be5eda6
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-14 11:29:17 +0530

    Day20 first question

---
+++ Day20.js
@@ -0,0 +1,20 @@
+// 🔹 Question 1 – Create and Call a Function
+// Write a JavaScript program to create a function named greet.
+// The function should print:
+// Hello, Welcome to JavaScript
+// ⚠️ Conditions
+// ✅ Create a function using function keyword
+// ✅ Function name should be greet
+// ✅ Use console.log() inside the function
+// ✅ Call the function
+// ❌ Don't use arrow function
+// ❌ Don't write the message directly outside the function
+// 💡 Hint: First define the function, then call it.
+
+function greet(){
+    console.log("Hello, Welcome to JavaScript");
+}
+
+greet();
+
+
