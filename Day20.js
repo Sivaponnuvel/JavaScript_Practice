b0d8581b4ebe1eddcde1de89bf826cca99163487
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-14 11:29:33 +0530

    Day20 second question

--- Day20.js
+++ Day20.js
@@ -18,3 +18,24 @@ function greet(){
 greet();
 
 
+// 🔹 Question 2 – Function with Parameter
+// Write a JavaScript program to create a function named greetUser that accepts a user's name as a parameter.
+// Call the function with:
+// Siva
+// The output should be:
+// Hello Siva
+// ⚠️ Conditions
+// ✅ Create a function using function keyword
+// ✅ Accept one parameter
+// ✅ Pass "Siva" while calling the function
+// ✅ Use console.log()
+// ❌ Don't use global variable for the name
+// ❌ Don't use arrow function
+// ❌ Don't hardcode "Hello Siva" inside console.log()
+// 💡 Hint: The parameter receives the value when you call the function.
+
+function greetUser(name){
+    console.log("Hello",name);
+}
+
+greetUser("Siva");
