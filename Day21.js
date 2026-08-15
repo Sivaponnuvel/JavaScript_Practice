2e3dc2e49a839c2e01854167aa2511952cb55bda
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-15 10:47:16 +0530

    Day21 first question

---
+++ Day21.js
@@ -0,0 +1,28 @@
+// 🔹 Question 1 – Function with Multiple Parameters
+// Write a JavaScript program to create a function named addNumbers that accepts two parameters.
+// The function should add the two numbers and display the result.
+// Program Flow
+// Create a function named addNumbers
+// Accept two parameters
+// Add the two parameters
+// Display the result using console.log()
+// Call the function with 10 and 20
+// Example Output
+// 30
+// ⚠️ Conditions
+// ✅ Use function keyword
+// ✅ Use two parameters
+// ✅ Use console.log() inside the function
+// ✅ Call the function with two values
+// ❌ Don't use global variables
+// ❌ Don't use arrow function
+// ❌ Don't hardcode 30
+// 💡 Hint: function addNumbers(a, b)
+
+function addNumbers(a, b){
+    console.log(a + b);
+}
+
+addNumbers(10, 20);
+
+
