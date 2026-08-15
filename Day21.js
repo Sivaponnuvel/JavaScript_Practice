d3d414ea8393c04882746672916a43e4157f4f67
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-15 10:47:30 +0530

    Day21 second question

--- Day21.js
+++ Day21.js
@@ -26,3 +26,33 @@ function addNumbers(a, b){
 addNumbers(10, 20);
 
 
+// 🔹 Question 2 – Function with return
+// Write a JavaScript program to create a function named square that accepts one number as a parameter and returns its square.
+// Call the function with 5 and display the returned result.
+// Program Flow
+// Create a function named square
+// Accept one parameter
+// Calculate the square
+// Use return
+// Store the returned value in a variable
+// Display the result using console.log()
+// Example Output
+// 25
+// ⚠️ Conditions
+// ✅ Use function keyword
+// ✅ Use one parameter
+// ✅ Use return
+// ✅ Call the function with 5
+// ✅ Display the returned value
+// ❌ Don't use console.log() inside the function
+// ❌ Don't hardcode 25
+// ❌ Don't use arrow function
+// 💡 Hint: The important difference is:
+// return result;
+
+function square(a){
+    let result = a**2;
+    return result;
+}
+
+console.log(square(5));
