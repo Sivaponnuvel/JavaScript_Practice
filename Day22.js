af84983c4354c1ee99883b1e062a64a39cb720e1
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-16 12:26:03 +0530

    Day22 second question

--- Day22.js
+++ Day22.js
@@ -32,3 +32,32 @@ function findLargest(a, b){
 console.log(findLargest(25, 40));
 
 
+// 🔹 Question 2 – Check Even or Odd Using Function
+// Write a JavaScript program to create a function named checkEvenOdd that accepts one number and returns:
+// "Even" if the number is even
+// "Odd" if the number is odd
+// Call the function with 17 and display the returned result.
+// Example Output
+// Odd
+// ⚠️ Conditions
+// ✅ Use function keyword
+// ✅ Use one parameter
+// ✅ Use return
+// ✅ Use % operator
+// ✅ Use if...else
+// ✅ Display the returned value using console.log()
+// ❌ Don't use global variables
+// ❌ Don't use arrow function
+// ❌ Don't hardcode "Odd" outside the function
+// 💡 Hint: A number is even when its remainder after dividing by 2 is 0.
+
+function checkEvenOdd(number){
+    if(number % 2 === 0){
+        return "Even";
+    }
+    else{
+        return "Odd";
+    }
+}
+
+console.log(checkEvenOdd(17))
