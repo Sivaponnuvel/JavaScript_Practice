4e1fd452221d39e212723878269b4c3c17b7cb07
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-18 17:06:02 +0530

    Day24 first question

---
+++ Day24.js
@@ -0,0 +1,31 @@
+// 🔹 Question 1 – Find String Length Using Function
+// Write a JavaScript program to create a function named findLength that accepts a string and returns its length.
+// Program Flow
+// Create a function named findLength
+// Accept one parameter
+// Find the length of the string
+// Use return
+// Call the function with "JavaScript"
+// Display the returned result using console.log()
+// Example Output
+// 10
+// ⚠️ Conditions
+// ✅ Use function keyword
+// ✅ Accept one parameter
+// ✅ Use .length
+// ✅ Use return
+// ✅ Display result using console.log()
+// ❌ Don't hardcode 10
+// ❌ Don't use arrow function
+// ❌ Don't use a global variable
+// 💡 Hint:
+// A string has a .length property.
+
+function findLength(string){
+    let result = string.length;
+    return result;
+}
+
+console.log(findLength("JavaScript"));
+
+
