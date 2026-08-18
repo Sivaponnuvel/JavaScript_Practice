85090c1211d5338cb76f35e0628b202b9aac8e24
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-18 17:06:36 +0530

    Day24 second question

--- Day24.js
+++ Day24.js
@@ -29,3 +29,32 @@ function findLength(string){
 console.log(findLength("JavaScript"));
 
 
+// 🔹 Question 2 – Convert String to Uppercase
+// Write a JavaScript program to create a function named convertUppercase that accepts a string and returns the string in uppercase.
+// Program Flow
+// Create a function named convertUppercase
+// Accept one parameter
+// Convert the string to uppercase
+// Use return
+// Call the function with "javascript"
+// Display the returned result using console.log()
+// Example Output
+// JAVASCRIPT
+// ⚠️ Conditions
+// ✅ Use function keyword
+// ✅ Accept one parameter
+// ✅ Use .toUpperCase()
+// ✅ Use return
+// ✅ Display the returned value using console.log()
+// ❌ Don't hardcode "JAVASCRIPT"
+// ❌ Don't use arrow function
+// ❌ Don't use a global variable
+// 💡 Hint:
+// There is a built-in string method that converts all characters to uppercase.
+
+function convertUppercase(language){
+    let result = language.toUpperCase();
+    return result;
+}
+
+console.log(convertUppercase("javascript"));
