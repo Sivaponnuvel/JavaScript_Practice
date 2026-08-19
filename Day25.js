e422be809ab03927bd512a4de6a7697d46b23072
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-19 09:52:36 +0530

    Day25 first question

---
+++ Day25.js
@@ -0,0 +1,29 @@
+// 🔹 Question 1 – Convert String to Lowercase
+// Write a JavaScript program to create a function named convertLowercase that accepts a string and returns the string in lowercase.
+// Program Flow
+// Create a function named convertLowercase
+// Accept one parameter
+// Convert the string to lowercase
+// Use return
+// Call the function with "JAVASCRIPT"
+// Display the returned result using console.log()
+// Example Output
+// javascript
+// ⚠️ Conditions
+// ✅ Use function keyword
+// ✅ Accept one parameter
+// ✅ Use .toLowerCase()
+// ✅ Use return
+// ✅ Use console.log()
+// ❌ Don't hardcode "javascript"
+// ❌ Don't use arrow function
+// ❌ Don't use global variable
+// 💡 Hint: Uppercase-ku .toUpperCase() use pannom. Lowercase-ku அதுக்கு opposite method என்னன்னு think pannu.
+
+function convertLowercase(string){
+    return string.toLowerCase();
+}
+
+console.log(convertLowercase("JAVASCRIPT"));
+
+
