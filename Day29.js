02d8626efe41bde6e8ead9f1999bcc2d27243eac
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-23 14:52:53 +0530

    Day29 second question

--- Day29.js
+++ Day29.js
@@ -20,3 +20,30 @@ let fruits = ["Apple", "Banana", "Mango", "Orange"];
 console.log(fruits);
 
 
+// 🔹 Question 2 – Access Array Elements
+// Create an array named languages containing:
+// "Python"
+// "JavaScript"
+// "Java"
+// "C++"
+// Display:
+// First element
+// Third element
+// Last element
+// Example Output
+// Python
+// Java
+// C++
+// ⚠️ Conditions
+// ✅ Use let
+// ✅ Use array indexing
+// ✅ Use console.log()
+// ❌ Don't hardcode the output directly inside console.log()
+// 💡 Hint: Array indexing starts from 0.
+// For the last element, think about using .length.
+
+let languages = ["Python", "JavaScript", "Java", "C++"]
+
+console.log(languages[0]);
+console.log(languages[2]);
+console.log(languages[languages.length - 1]);
