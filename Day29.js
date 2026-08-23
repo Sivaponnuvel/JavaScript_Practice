0f3f9e5476944c213d1c34605a0583d8d20f5216
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-23 14:52:32 +0530

    Day29 first question

---
+++ Day29.js
@@ -0,0 +1,22 @@
+// 🔹 Question 1 – Create and Display an Array
+// Create an array named fruits containing these values:
+// "Apple"
+// "Banana"
+// "Mango"
+// "Orange"
+// Then display the entire array using console.log().
+// Example Output
+// ["Apple", "Banana", "Mango", "Orange"]
+// ⚠️ Conditions
+// ✅ Use let
+// ✅ Create an array
+// ✅ Store all 4 values
+// ✅ Use console.log()
+// ❌ Don't use four separate variables
+// 💡 Hint: Arrays use square brackets [].
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+console.log(fruits);
+
+
