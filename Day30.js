8848be6277ea59ef612fc10eedfe6b92c5e184c5
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-24 15:14:38 +0530

    Day30 first question

---
+++ Day30.js
@@ -0,0 +1,27 @@
+// 🔹 Question 1 – Add Elements Using .push()
+// Create an array named colors with:
+// "Red"
+// "Blue"
+// Then:
+// Add "Green" using .push()
+// Add "Yellow" using .push()
+// Display the final array.
+// Example Output
+// ["Red", "Blue", "Green", "Yellow"]
+// ⚠️ Conditions
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .push()
+// ✅ Use console.log()
+// ❌ Don't directly add all four values while creating the array
+// ❌ Don't use unshift()
+// 💡 Hint: .push() adds a new element at the end of an array.
+
+let colors = ["Red", "Blue",];
+
+colors.push("Green");
+colors.push("Yellow");
+
+console.log(colors);
+
+
