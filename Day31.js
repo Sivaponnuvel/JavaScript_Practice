449c2287da5b4cefa17ae16fa7c95c32580499c6
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-25 10:41:05 +0530

    Day31 second question

--- Day31.js
+++ Day31.js
@@ -30,3 +30,27 @@ console.log(remove);
 console.log(fruits);
 
 
+// 🔹 Question 2 – Add Elements Using .unshift()
+// Create an array named numbers with:
+// 30, 40
+// Then:
+// Add 20 at the beginning using .unshift()
+// Add 10 at the beginning using .unshift()
+// Display the final array
+// Example Output
+// [10, 20, 30, 40]
+// ⚠️ Conditions
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .unshift()
+// ✅ Use console.log()
+// ❌ Don't directly create the array with all four values
+// ❌ Don't use .push()
+// 💡 Hint: .unshift() adds a new element at the beginning of an array. Since you're adding one element at a time, think about what happens to the existing elements each time.
+
+let numbers = [30, 40];
+
+numbers.unshift(20);
+numbers.unshift(10);
+
+console.log(numbers);
