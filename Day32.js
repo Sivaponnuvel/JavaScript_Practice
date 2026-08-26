625e9b84d26782bd33cf054adaf695ec1a08eb90
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-26 09:43:35 +0530

    Day32 first question

---
+++ Day32.js
@@ -0,0 +1,23 @@
+// 🔹 Question 1 – Find Number of Elements
+// Create an array named cities containing:
+// "Chennai"
+// "Mumbai"
+// "Delhi"
+// "Bangalore"
+// "Kolkata"
+// Display the total number of elements in the array.
+// Example Output
+// 5
+// ⚠️ Conditions
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .length
+// ✅ Use console.log()
+// ❌ Don't hardcode 5
+// 💡 Hint: Array-la total items count panna .length use pannalam.
+
+let cities =  ['Chennai', "Mumbai", "Delhi", "Bangalore", "Kolkata"];
+
+console.log(cities.length);
+
+
