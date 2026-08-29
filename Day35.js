bc5c4aab52db0a87449a1cc4f8974d0d0e13e33f
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-29 12:30:25 +0530

    Day35 second question

--- Day35.js
+++ Day35.js
@@ -19,3 +19,24 @@ let result =  fruits1.concat(fruits2);
 console.log(result);
 
 
+// 🔹 Question 2 – Combine Three Arrays
+// Create:
+// let numbers1 = [10, 20];
+// let numbers2 = [30, 40];
+// let numbers3 = [50, 60];
+// Using .concat(), combine all three arrays into one array.
+// Expected Output:
+// [10, 20, 30, 40, 50, 60]
+// ⚠️ Conditions:
+// ✅ Use .concat()
+// ✅ Store the result in a new variable
+// ❌ Don't manually create the final array
+// ❌ Don't use .push()
+
+let numbers1 = [10, 20];
+let numbers2 = [30, 40];
+let numbers3 = [50, 60];
+
+let answer = numbers1.concat(numbers2, numbers3);
+
+console.log(answer);
