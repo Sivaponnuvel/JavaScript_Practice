d6b63e641c1f77ea67b384c90f776fa4adb3ca68
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-29 12:30:00 +0530

    Day35 first question

---
+++ Day35.js
@@ -0,0 +1,21 @@
+// 🔹 Question 1 – Combine Two Arrays
+// Create:
+// let fruits1 = ["Apple", "Banana"];
+// let fruits2 = ["Mango", "Orange"];
+// Using .concat(), combine both arrays into a new variable.
+// Expected Output:
+// ["Apple", "Banana", "Mango", "Orange"]
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use .concat()
+// ✅ Store the result in a new variable
+// ❌ Don't use .push()
+
+let fruits1 = ["Apple", "Banana"];
+let fruits2 = ["Mango", "Orange"];
+
+let result =  fruits1.concat(fruits2);
+
+console.log(result);
+
+
