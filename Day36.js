4b281e520fbda78324a2a69862e4d70615829ba7
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-30 10:42:50 +0530

    Day36 first question

---
+++ Day36.js
@@ -0,0 +1,23 @@
+// 🔹 Question 1 – Create a Function
+// Create a function named greet that takes a name as a parameter.
+// Then:
+// Pass "Siva" to the function
+// Return "Hello Siva"
+// Display the returned value.
+// Expected Output:
+// Hello Siva
+// ⚠️ Conditions:
+// ✅ Use function
+// ✅ Function name should be greet
+// ✅ Use one parameter
+// ✅ Use return
+// ✅ Use console.log()
+// ❌ Don't use arrow function
+
+function greet(name){
+    return "Hello " + name;
+}
+
+console.log(greet("Siva"));
+
+
