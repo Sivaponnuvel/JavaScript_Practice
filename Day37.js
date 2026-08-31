22314b9d2bda74c625177c5da9e729747cb657de
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-31 09:52:25 +0530

    Day37 second question

--- Day37.js
+++ Day37.js
@@ -20,3 +20,30 @@ const calculateTotal = (a, b, c) =>{
 }
 console.log(calculateTotal(10, 20, 30));
 
+
+// 🔹 Question 2 – Arrow Function with One Parameter
+// Create an arrow function named isEven that takes one number as a parameter.
+// Then:
+// Pass 8 to the function
+// Check whether the number is even
+// Return true if it is even
+// Display the result
+// Expected Output:
+// true
+// 💡 Hint:
+// Use the modulus operator %.
+// number % 2 === 0
+// ⚠️ Conditions:
+// ✅ Use const
+// ✅ Use arrow function =>
+// ✅ Use one parameter
+// ✅ Use return
+// ✅ Use console.log()
+// ❌ Don't use if...else
+// ❌ Don't use normal function syntax
+
+const isEven = (number) => {
+    return number % 2 === 0;
+}
+
+console.log(isEven(8));
