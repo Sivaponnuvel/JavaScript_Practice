1aad0f7f140cf1858b98a450e0205cc5c5af9209
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-31 09:51:58 +0530

    Day37 first question

---
+++ Day37.js
@@ -0,0 +1,22 @@
+// 🔹 Question 1 – Arrow Function with Multiple Parameters
+// Create an arrow function named calculateTotal that takes three numbers as parameters.
+// Then:
+// Pass 10, 20, and 30 to the function
+// Add all three numbers
+// Return the result
+// Display the result
+// Expected Output:
+// 60
+// ⚠️ Conditions:
+// ✅ Use const
+// ✅ Use arrow function =>
+// ✅ Use three parameters
+// ✅ Use return
+// ✅ Use console.log()
+// ❌ Don't use normal function syntax
+
+const calculateTotal = (a, b, c) =>{
+    return a + b + c;
+}
+console.log(calculateTotal(10, 20, 30));
+
