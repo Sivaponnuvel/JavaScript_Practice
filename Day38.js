9ffe9d9a1fefc8f303643870177cdcc93b467b2f
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-01 10:27:34 +0530

    Day38 first question

---
+++ Day38.js
@@ -0,0 +1,27 @@
+// 🔹 Question 1 – Print Each Element
+// Create:
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// Using forEach() with an arrow function:
+// Print each fruit individually.
+// Each fruit should appear on a separate line.
+// Expected Output:
+// Apple
+// Banana
+// Mango
+// Orange
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .forEach()
+// ✅ Use arrow function =>
+// ✅ Use console.log()
+// ❌ Don't use for loop
+// ❌ Don't use while loop
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+fruits.forEach((fruits) => {
+    console.log(fruits);
+})
+
+
