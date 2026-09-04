a14830838abf3a2e05e133b8209677942988f597
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-04 12:29:57 +0530

    Day41 second question

--- Day41.js
+++ Day41.js
@@ -29,3 +29,34 @@ const result = numbers.reduce((sum, number) => {
 console.log(result);
 
 
+// 🔹 Question 2 – Calculate Product
+// Create:
+// let numbers = [2, 3, 4, 5];
+// Using reduce() with an arrow function:
+// Multiply all numbers together
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// 120
+// 💡 Hint:
+// For multiplication, use an initial value of 1.
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use .reduce()
+// ✅ Use arrow function =>
+// ✅ Use an accumulator
+// ✅ Use an initial value of 1
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use forEach()
+// ❌ Don't use map()
+// ❌ Don't use filter()
+// ❌ Don't use a for loop
+
+let numbers1 = [2, 3, 4, 5];
+
+const answer = numbers1.reduce((product, number) => {
+    return product * number;
+}, 1);
+
+console.log(answer);
