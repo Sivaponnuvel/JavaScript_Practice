5350f72e7bb3f0ae5bc2a7b336ba2321d2681488
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-06 17:29:56 +0530

    Day43 second question

--- Day43.js
+++ Day43.js
@@ -27,3 +27,31 @@ const result = numbers.findIndex((number) => {
 console.log(result);
 
 
+// 🔹 Question 2 – Find Name Index
+// Create:
+// let names = ["Arun", "Kumar", "Siva", "Vijay", "Raj"];
+// Using findIndex() with an arrow function:
+// Find the index of the first name whose length is greater than 4
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// 1
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use .findIndex()
+// ✅ Use arrow function =>
+// ✅ Use .length
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use find()
+// ❌ Don't use filter()
+// ❌ Don't use forEach()
+// ❌ Don't manually select the index
+
+let names = ["Arun", "Kumar", "Siva", "Vijay", "Raj"];
+
+const answer = names.findIndex((name)=>{
+    return name.length > 4;
+});
+
+console.log(answer)
