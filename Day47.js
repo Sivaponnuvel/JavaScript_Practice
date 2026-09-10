d16a4ffce3851a394d65db634d04f3fd5de156b1
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-10 12:11:18 +0530

    Day47 second question

--- Day47.js
+++ Day47.js
@@ -27,3 +27,30 @@ const result = numbers.reverse((number) => {
 console.log(result)
 
 
+// 🔹 Question 2 – Reverse a String Array
+// Create:
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// Using .reverse():
+// Reverse the order of the fruits
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// ["Orange", "Mango", "Banana", "Apple"]
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .reverse()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use for loop
+// ❌ Don't use forEach()
+// ❌ Don't use map()
+// ❌ Don't create a new array manually
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+const answer = fruits.reverse((fruit) => {
+    return fruit;
+});
+
+console.log(answer);
