4b3d6fce857199ef4684e642d8d4dd996fa7e791
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-11 16:43:16 +0530

    Day48 second question

--- Day48.js
+++ Day48.js
@@ -25,3 +25,28 @@ const result = fruits.includes("Mango");
 console.log(result);
 
 
+// 🔹 Question 2 – Check if an Element Does Not Exist
+// Create:
+// let colors = ["Red", "Blue", "Green", "Yellow"];
+// Using .includes():
+// Check whether "Purple" exists in the array
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// false
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .includes()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use for loop
+// ❌ Don't use forEach()
+// ❌ Don't use find()
+// ❌ Don't use indexOf()
+
+let colors = ["Red", "Blue", "Green", "Yellow"];
+
+let answer = colors.includes("Purple");
+
+console.log(answer);
