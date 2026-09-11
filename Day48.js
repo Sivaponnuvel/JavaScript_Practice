2fde46d7a1368df7a34fba7e6b185e0492a07382
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-11 16:34:56 +0530

    Day48 first question

---
+++ Day48.js
@@ -0,0 +1,27 @@
+// 🔹 Question 1 – Check if an Element Exists
+// Create:
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// Using .includes():
+// Check whether "Mango" exists in the array
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// true
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .includes()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use for loop
+// ❌ Don't use forEach()
+// ❌ Don't use find()
+// ❌ Don't use indexOf()
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+const result = fruits.includes("Mango");
+
+console.log(result);
+
+
