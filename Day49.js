a36c8a82dea612f402c20cf45124824810ba9d78
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-12 10:40:36 +0530

    Day49 second question

--- Day49.js
+++ Day49.js
@@ -26,3 +26,29 @@ const result = products.join(" | ")
 console.log(result);
 
 
+// 🔹 Question 2 – Create a Breadcrumb
+// You are building a website navigation bar. The current page path is stored as:
+// let path = ["Home", "Products", "Electronics", "Laptops"];
+// Using .join():
+// Combine the items into a breadcrumb
+// Separate each item with " > "
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// Home > Products > Electronics > Laptops
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .join()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use for loop
+// ❌ Don't use forEach()
+// ❌ Don't use map()
+// ❌ Don't manually create the final string
+
+let path = ["Home", "Products", "Electronics", "Laptops"];
+
+const answer = path.join(" > ");
+
+console.log(answer);
