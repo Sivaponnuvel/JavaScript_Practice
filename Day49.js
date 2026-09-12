f8730675c3e24be7706e69fed40e5d0194f81dc8
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-12 10:40:11 +0530

    Day49 first question

---
+++ Day49.js
@@ -0,0 +1,28 @@
+// 🔹 Question 1 – Create a Product List for Display
+// You are building an e-commerce website. You have:
+// let products = ["Laptop", "Mouse", "Keyboard", "Monitor"];
+// Using .join():
+// Combine all product names into one string
+// Separate each product with " | "
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// Laptop | Mouse | Keyboard | Monitor
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an array
+// ✅ Use .join()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use for loop
+// ❌ Don't use forEach()
+// ❌ Don't use map()
+// ❌ Don't manually create the final string
+
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor"];
+
+const result = products.join(" | ")
+
+console.log(result);
+
+
