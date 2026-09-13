bea2a2c85f86ac7352db7536ba6e37753a1e1ea4
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-13 17:46:14 +0530

    Day50 first question

---
+++ Day50.js
@@ -0,0 +1,25 @@
+// 🔹 Question 1 – Clean User Name
+// You are building a registration form. A user entered their name with unwanted spaces:
+// let username = "   Sivaponnuvel   ";
+// Using .trim():
+// Remove the extra spaces from the beginning and end
+// Store the cleaned username in a new variable
+// Display it
+// Expected Output:
+// Sivaponnuvel
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use .trim()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't manually remove the spaces
+// ❌ Don't use replace()
+// ❌ Don't use a loop
+
+let username = "   Sivaponnuvel   ";
+
+let result = username.trim();
+
+console.log(result);
+
+
