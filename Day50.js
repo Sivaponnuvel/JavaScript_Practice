dbe94d42fe1735193985702d1b4610a1a7aeb688
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-13 17:46:40 +0530

    Day50 second question

--- Day50.js
+++ Day50.js
@@ -23,3 +23,25 @@ let result = username.trim();
 console.log(result);
 
 
+// 🔹 Question 2 – Clean Search Input
+// You are building a product search box. The user entered:
+// let search = "   laptop   ";
+// Using .trim():
+// Remove the unwanted spaces
+// Store the cleaned value in a new variable
+// Display the cleaned search value
+// Expected Output:
+// laptop
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use .trim()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use replace()
+// ❌ Don't use a loop
+
+let search = "   laptop   ";
+
+let answer = search.trim();
+
+console.log(answer); 
