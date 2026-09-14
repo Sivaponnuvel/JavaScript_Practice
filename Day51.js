7f717167700625115267fccd9b29be070be511a3
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-14 19:32:05 +0530

    Day51 first question

---
+++ Day51.js
@@ -0,0 +1,25 @@
+// 🔹 Question 1 – Format Username
+// You are building a user profile page. The username is stored as:
+// let username = "sivaponnuvel";
+// Using .toUpperCase():
+// Convert the username to uppercase
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// SIVAPONNUVEL
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use .toUpperCase()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use .toLowerCase()
+// ❌ Don't manually change the letters
+// ❌ Don't use a loop
+
+let username = "sivaponnuvel";
+
+let result = username.toUpperCase();
+
+console.log(result);
+
+
