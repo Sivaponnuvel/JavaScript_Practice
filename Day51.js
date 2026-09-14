084b77db7a663d28033f67218cc19589338e33fe
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-14 19:32:30 +0530

    Day51 second question

--- Day51.js
+++ Day51.js
@@ -23,3 +23,26 @@ let result = username.toUpperCase();
 console.log(result);
 
 
+// 🔹 Question 2 – Format Product Code
+// You are building an inventory system. A product code is entered as:
+// let productCode = "lap2026";
+// Using .toUpperCase():
+// Convert the product code to uppercase
+// Store the result in a new variable
+// Display the result
+// Expected Output:
+// LAP2026
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use .toUpperCase()
+// ✅ Store the result in a new variable
+// ✅ Use console.log()
+// ❌ Don't use .toLowerCase()
+// ❌ Don't manually change the letters
+// ❌ Don't use a loop
+
+let productCode = "lap2026";
+
+let answer = productCode.toUpperCase();
+
+console.log(answer);
