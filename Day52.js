3ec9771f251fb91fe3e14ef219f0694cef10aaca
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-15 09:12:19 +0530

    Day52 second question

--- Day52.js
+++ Day52.js
@@ -26,3 +26,29 @@ function showWelcome(name){
 loginUser("Siva", showWelcome);
 
 
+// 🔹 Question 2 – Order Processing Callback
+// You are building an online shopping system.
+// Create a function processOrder that:
+// Accepts an orderId and a callback function
+// Calls the callback with the orderId
+// Call it with:
+// "ORD101"
+// The callback should display:
+// Order ORD101 processed successfully
+// ⚠️ Conditions:
+// ✅ Use a normal function
+// ✅ Pass a callback function as an argument
+// ✅ Call the callback inside processOrder()
+// ✅ Use console.log()
+// ❌ Don't use arrow functions
+// ❌ Don't use global variables
+
+function processOrder(orderId, callback){
+    callback(orderId);
+}
+
+function showOrder(id){
+    console.log(`Order ${id} processed successfully`);
+}
+
+processOrder("ORD101", showOrder);
