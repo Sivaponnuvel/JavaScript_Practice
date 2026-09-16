442a0ba9490b130c49c3ef3268d7b288c8b73fd2
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-16 12:03:59 +0530

    Day53 second question

--- Day53.js
+++ Day53.js
@@ -27,3 +27,31 @@ function showSuccess(amount){
 makePayment(500, showSuccess);
 
 
+// 🔹 Question 2 – Delivery Status Callback
+// You are building an online food delivery application.
+// Create a function updateDeliveryStatus that:
+// Accepts an orderId, status, and a callback function
+// Calls the callback with both orderId and status
+// Call it with:
+// "ORD205"
+// "Delivered"
+// The callback should display:
+// Order ORD205 status: Delivered
+// ⚠️ Conditions:
+// ✅ Use a normal function
+// ✅ Pass a callback function as an argument
+// ✅ Call the callback inside updateDeliveryStatus()
+// ✅ Pass both values to the callback
+// ✅ Use console.log()
+// ❌ Don't use arrow functions
+// ❌ Don't use global variables
+
+function updateDeliveryStatus(orderId, status, callback){
+    callback(orderId, status);
+}
+
+function showStatus(id, status){
+    console.log(`Order ${id} status: ${status}`);
+}
+
+updateDeliveryStatus("ORD205", "Delivered", showStatus);
