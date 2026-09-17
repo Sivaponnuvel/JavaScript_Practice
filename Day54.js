4f5138887d783b36796077b5cd555afbc2c7c99e
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-17 19:43:30 +0530

    Day54 second question

--- Day54.js
+++ Day54.js
@@ -29,3 +29,43 @@ console.log(properties.name);
 console.log(properties.role);
 
 
+// 🔹 Question 2 – Update Product Price
+// You are building an e-commerce application.
+// Create an object:
+// let product = {
+//     name: "Laptop",
+//     price: 55000,
+//     category: "Electronics"
+// };
+// The product price has been updated to ₹60000.
+// Using the object:
+// Update the price property to 60000
+// Display the updated price
+// Display the complete product object
+// Expected Output:
+// 60000
+// {
+//     name: "Laptop",
+//     price: 60000,
+//     category: "Electronics"
+// }
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an object
+// ✅ Update the existing property
+// ✅ Use dot notation (.)
+// ✅ Use console.log()
+// ❌ Don't create a second object
+// ❌ Don't use an array
+// ❌ Don't use a loop
+
+let product = {
+    name: "Laptop",
+    price: 55000,
+    category: "Electronics"
+};
+
+product.price = 60000
+
+console.log(product.price);
+console.log(product);
