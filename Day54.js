ddbda17c99bfad4d82745593edcd0d4a3a88cad5
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-17 19:43:01 +0530

    Day54 first question

---
+++ Day54.js
@@ -0,0 +1,31 @@
+// 🔹 Question 1 – User Profile
+// You are building a user profile section for a web application.
+// Create an object named user with these properties:
+// name → "Sivaponnuvel"
+// age → 22
+// role → "Python Developer"
+// Then:
+// Display the user's name
+// Display the user's role
+// Expected Output:
+// Sivaponnuvel
+// Python Developer
+// ⚠️ Conditions:
+// ✅ Use let
+// ✅ Use an object
+// ✅ Use property-value pairs
+// ✅ Access properties using dot notation (.)
+// ✅ Use console.log()
+// ❌ Don't use an array
+// ❌ Don't use a loop
+
+let properties = {
+    name: "Sivaponnuvel",
+    age: 22,
+    role: "Python Developer"
+};
+
+console.log(properties.name);
+console.log(properties.role);
+
+
