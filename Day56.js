cdd1d3ea5e9b343923b2790020c4256105722a2f
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-19 09:43:47 +0530

    Day56 second question

--- Day56.js
+++ Day56.js
@@ -32,3 +32,35 @@ const emp = new Employee("Sivaponnuvel", "Python Developer", 30000);
 emp.display();
 
 
+// 🟢 Question 2 – Product Management
+// Create a class called Product.
+// The constructor should accept:
+// name
+// price
+// category
+// Create a product object with:
+// Name: Laptop
+// Price: 55000
+// Category: Electronics
+// Then print:
+// Product: Laptop
+// Price: 55000
+// Category: Electronics
+// Condition: Create the object using new Product(...) and access the values using object properties.
+
+class Product{
+    constructor(name, price, category){
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+    showAnswer(){
+        console.log(`Product: ${this.name}`);
+        console.log(`Price: ${this.price}`);
+        console.log(`Category: ${this.category}`);
+    }
+}
+
+const pro = new Product("Laptop", 55000, "Electronics");
+
+pro.showAnswer();
