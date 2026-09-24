192a51d99f097ed9cd147d87ca7fa5cfd725b4f3
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-24 11:57:32 +0530

    Day61 first question

---
+++ Day61.js
@@ -0,0 +1,22 @@
+// 🟢 Question 1 – Employee Joining Date
+// An employee joined a company on:
+// let joiningDate = new Date("2025-06-15");
+// Do the following:
+// Print the complete joiningDate
+// Print the joining year
+// Print the joining month
+// Print the joining date
+// Expected concept:
+// 2025
+// 5
+// 15
+// Note: JavaScript getMonth() starts from 0, so June = 5.
+
+let joiningDate = new Date("2025-06-15");
+
+console.log(joiningDate);
+console.log(joiningDate.getFullYear());
+console.log(joiningDate.getMonth());
+console.log(joiningDate.getDate());
+
+
