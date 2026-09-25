f3ef51e5690c485a1b37ec81fcf8c2df14f9afb4
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-25 17:10:33 +0530

    Day62 second question

--- Day62.js
+++ Day62.js
@@ -23,3 +23,27 @@ console.log(appointmentDate.getMinutes());
 console.log(`${appointmentDate.getDate()}/${appointmentDate.getMonth()+1}/${appointmentDate.getFullYear()}`);
 
 
+// 🟢 Question 2 – Set a Future Deadline
+// A developer has a project deadline:
+// let deadline = new Date("2026-09-23");
+// Using JavaScript Date methods:
+// Change the year to 2027
+// Change the month to December
+// Change the date to 25
+// Print the final date.
+// Print the year, month, and date separately.
+// Expected concept:
+// 2027
+// 11
+// 25
+// Remember: December = 11 because JavaScript months start from 0.
+
+let deadline = new Date("2026-09-23");
+
+deadline.setFullYear(2027);
+deadline.setMonth(11);
+deadline.setDate(25);
+
+console.log(deadline.getFullYear());
+console.log(deadline.getMonth());
+console.log(deadline.getDate());
