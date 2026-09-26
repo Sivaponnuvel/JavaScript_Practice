b4068108734fab351c0e5a2ef9ceedb49dc59111
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-26 14:40:32 +0530

    Day63 second question

--- Day63.js
+++ Day63.js
@@ -23,3 +23,27 @@ function countdown(n){
 countdown(5);
 
 
+// 🟢 Question 2 – Calculate Factorial
+// Create a recursive function called factorial() that calculates the factorial of a number.
+// Example:
+// factorial(5);
+// Expected Output:
+// 120
+// Because:
+// 5 × 4 × 3 × 2 × 1 = 120
+// Test your function with:
+// factorial(6);
+// Expected Output:
+// 720
+// Condition: Use recursion.
+// Do not use loops.
+
+function factorial(a) {
+    if (a <= 1){
+        return 1;
+    }
+    return a * factorial(a - 1);
+}
+
+console.log(factorial(5));
+console.log(factorial(6));
